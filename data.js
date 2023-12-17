6f0cafcea43ad93f6132587b86163ac520db7f8f
Newrred <104729937+Newrred@users.noreply.github.com>
2023-12-17 23:47:05 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,55 +1,55 @@
 const qnaList = [
     {
-    url: "q_img/q01.png", url2:"q_img/q01_1.png",url3:"q_img/q01_2.png",
+    url: "./q_img/q01.png", url2:"./q_img/q01_1.png",url3:"./q_img/q01_2.png",
     q: '당신은 어떤 상품을 구매하시겠습니까?',
     a: [
         {answer: 'A', type:['right'], key:['1']},{answer: 'B', type:['wrong'], key:['0']}
     ]
 },
 {
-    url: "q_img/q02.png", url2:"q_img/q02_1.png",url3:"q_img/q02_2.png",
+    url: "./q_img/q02.png", url2:"./q_img/q02_1.png",url3:"./q_img/q02_2.png",
     q: '당신은 어떤 쿠폰을 사용하시겠습니까?',
     a: [
         {answer: 'A', type:['wrong'], key:['0']},{answer: 'B', type:['right'], key:['1']}
     ]
 },
 {
-    url: "q_img/q03.png", url2:"q_img/q03_1.png",url3:"q_img/q03_2.png",
+    url: "./q_img/q03.png", url2:"./q_img/q03_1.png",url3:"./q_img/q03_2.png",
     q: '결제 페이지를 선택하세요.',
     a: [
         {answer: 'A', type:['wrong'], key:['0']},{answer: 'B', type:['right'], key:['1']}
     ]
 },
 {
-    url: "q_img/q04.png", url2:"q_img/q04_1.png",url3:"q_img/q04_2.png",
+    url: "./q_img/q04.png", url2:"./q_img/q04_1.png",url3:"./q_img/q04_2.png",
     q: '어떤 버튼을 클릭하시겠습니까?',
     a: [
         {answer: 'A', type:['right'], key:['1']},{answer: 'B', type:['wrong'], key:['0']}
     ]
 },
 {
-    url: "q_img/q05.png", url2:"q_img/q05_1.png",url3:"q_img/q05_2.png",
+    url: "./q_img/q05.png", url2:"./q_img/q05_1.png",url3:"./q_img/q05_2.png",
     q: '당신은 어떤 쿠폰을 사용하시겠습니까?',
     a: [
         {answer: 'A', type:['wrong'], key:['0']},{answer: 'B', type:['right'], key:['1']}
     ]
 },
 {
-    url: "q_img/q06.png", url2:"q_img/q06_1.png",url3:"q_img/q06_2.png",
+    url: "./q_img/q06.png", url2:"./q_img/q06_1.png",url3:"./q_img/q06_2.png",
     q: '구매할 상품을 선택하세요 ',
     a: [
         {answer: 'A', type:['wrong'], key:['0']},{answer: 'B', type:['right'], key:['1']}
     ]
 },
 {
-    url: "q_img/q07.png", url2:"q_img/q07_1.png",url3:"q_img/q07_2.png",
+    url: "./q_img/q07.png", url2:"./q_img/q07_1.png",url3:"./q_img/q07_2.png",
     q: '어떤 상품을 클릭하시겠습니까? ',
     a: [
         {answer: 'A', type:['wrong'], key:['0']},{answer: 'B', type:['right'], key:['1']}
     ]
 },
 {
-    url: "q_img/q08.png", url2:"q_img/q08_1.png",url3:"q_imq08_2.png",
+    url: "./q_img/q08.png", url2:"./q_img/q08_1.png",url3:"./q_imq08_2.png",
     q: '어떤 상품을 클릭하시겠습니까? ',
     a: [
         {answer: 'A', type:['wrong']},{answer: 'B', type:['right']}
@@ -58,14 +58,14 @@ const qnaList = [
 ];
 
 const resultImgRoot = [
-    {uu:'q_img/a01.png'},
-    {uu:'q_img/a02.png'},
-    {uu:'q_img/a03.png'},
-    {uu:'q_img/a04.png'},
-    {uu:'q_img/a05.png'},
-    {uu:'q_img/a06.png'},
-    {uu:'q_img/a07.png'},
-    {uu:'q_img/a08.png'}
+    {uu:'./q_img/a01.png'},
+    {uu:'./q_img/a02.png'},
+    {uu:'./q_img/a03.png'},
+    {uu:'./q_img/a04.png'},
+    {uu:'./q_img/a05.png'},
+    {uu:'./q_img/a06.png'},
+    {uu:'./q_img/a07.png'},
+    {uu:'./q_img/a08.png'}
 
 ];
 
